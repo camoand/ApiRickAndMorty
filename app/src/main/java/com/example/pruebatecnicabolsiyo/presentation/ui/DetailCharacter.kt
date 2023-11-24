81aeb77e2c35000bf7ca1b0dbe57c570cffca075
AndresDiaz0894 <andresdiaz@wposs.com>
2023-11-24 10:36:38 -0500

    REFACTORIZACIÓN - Se optimiza la clase DetailCharacter.kt

--- app/src/main/java/com/example/pruebatecnicabolsiyo/presentation/ui/DetailCharacter.kt
+++ app/src/main/java/com/example/pruebatecnicabolsiyo/presentation/ui/DetailCharacter.kt
@@ -1,9 +1,7 @@
 package com.example.pruebatecnicabolsiyo.presentation.ui
 
-import android.app.Activity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,7 +21,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
@@ -39,210 +36,112 @@ import com.example.pruebatecnicabolsiyo.presentation.viewmodel.ApiViewModel
 
 @Composable
 fun ViewDetailsCharacter(apiViewModel: ApiViewModel, navController: NavHostController, id: Int) {
-    val activity = LocalContext.current as Activity
     val charactersStates by apiViewModel.state.collectAsState()
-    Box(
-        Modifier
-            .fillMaxSize(),
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Close,
+            contentDescription = "Close",
+            tint = Color.Black,
+            modifier = Modifier
+                .align(Alignment.End)
+                .clickable {
+                    navController.navigate(Routes.Pantalla1.routes) {
+                        popUpTo(
+                            Routes.Pantalla2.routes
+                        ) {
+                            inclusive = true
+                        }
+                    }
+                }
+        )
+        Spacer(modifier = Modifier.padding(5.dp))
+        Card(
+            shape = MaterialTheme.shapes.medium,
+            modifier = Modifier
+                .height(200.dp)
+                .width(160.dp)
         ) {
+            AsyncImage(
+                model = charactersStates.character!!.results[id].image,
+                contentDescription = "photoCharacter",
+                contentScale = ContentScale.FillHeight,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        Spacer(modifier = Modifier.padding(4.dp))
+        Text(
+            text = charactersStates.character!!.results[id].name,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Monospace,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.padding(5.dp))
         Column(
             Modifier
                 .fillMaxSize()
-                .background(Color.White)
-                .padding(8.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(start = 8.dp)
         ) {
-            Icon(
-                imageVector = Icons.Filled.Close,
-                contentDescription = "Close",
-                tint = Color.Black,
-                modifier = Modifier
-                    .align(Alignment.End)
-                    .clickable {
-                        navController.navigate(Routes.Pantalla1.routes) {
-                            popUpTo(
-                                Routes.Pantalla2.routes
-                            ) {
-                                inclusive = true
-                            }
-                        }
-                    }
+            TextViewDescription(
+                atribute = "Origen: ",
+                description = charactersStates.character!!.results[id].origin.name
             )
-            Spacer(modifier = Modifier.padding(5.dp))
-            Card(
-                shape = MaterialTheme.shapes.medium,
-                modifier = Modifier
-                    .height(200.dp)
-                    .width(160.dp)
-            ) {
-                AsyncImage(
-                    model = charactersStates.character!!.results[id].image,
-                    contentDescription = "photoCharacter",
-                    contentScale = ContentScale.FillHeight,
-                    modifier = Modifier.fillMaxSize()
-                )
-            }
-            Spacer(modifier = Modifier.padding(4.dp))
-            Text(
-                text = charactersStates.character!!.results[id].name,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.Monospace,
-                textAlign = TextAlign.Center
+            TextViewDescription(
+                atribute = "Especie: ",
+                description = charactersStates.character!!.results[id].species
             )
-            Spacer(modifier = Modifier.padding(5.dp))
-            Column(
-                Modifier
-                    .fillMaxSize()
-                    .padding(start = 8.dp)
-            ) {
-                Spacer(modifier = Modifier.padding(8.dp))
-
-                Text(
-                    buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                fontSize = 18.sp,
-                                fontWeight = FontWeight.Bold,
-                                fontFamily = FontFamily.Monospace,
-                            )
-                        ) {
-                            append("Origen:")
-                        }
-                        withStyle(
-                            style = SpanStyle(
-                                fontSize = 15.sp,
-                                fontWeight = FontWeight.SemiBold,
-                                fontFamily = FontFamily.Monospace,
-                            )
-                        ) {
-                            append(charactersStates.character!!.results[id].origin.name)
-                        }
-
-                    }
-                )
-                Spacer(modifier = Modifier.padding(8.dp))
-                Text(
-                    buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                fontSize = 18.sp,
-                                fontWeight = FontWeight.Bold,
-                                fontFamily = FontFamily.Monospace,
-                            )
-                        ) {
-                            append("Especie:")
-                        }
-                        withStyle(
-                            style = SpanStyle(
-                                fontSize = 15.sp,
-                                fontWeight = FontWeight.SemiBold,
-                                fontFamily = FontFamily.Monospace,
-                            )
-                        ) {
-                            append(charactersStates.character!!.results[id].species)
-                        }
-
-                    }
-                )
-                Spacer(modifier = Modifier.padding(8.dp))
-                Text(
-                    buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                fontSize = 18.sp,
-                                fontWeight = FontWeight.Bold,
-                                fontFamily = FontFamily.Monospace,
-                            )
-                        ) {
-                            append("Genero:")
-                        }
-                        withStyle(
-                            style = SpanStyle(
-                                fontSize = 15.sp,
-                                fontWeight = FontWeight.SemiBold,
-                                fontFamily = FontFamily.Monospace,
-                            )
-                        ) {
-                            append(charactersStates.character!!.results[id].gender)
-                        }
-
-                    }
-                )
-                Spacer(modifier = Modifier.padding(8.dp))
-                Text(
-                    buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                fontSize = 18.sp,
-                                fontWeight = FontWeight.Bold,
-                                fontFamily = FontFamily.Monospace,
-                            )
-                        ) {
-                            append("tipo:")
-                        }
-                        withStyle(
-                            style = SpanStyle(
-                                fontSize = 15.sp,
-                                fontWeight = FontWeight.SemiBold,
-                                fontFamily = FontFamily.Monospace,
-                            )
-                        ) {
-                            append(charactersStates.character!!.results[id].type)
-                        }
-
-                    }
-                )
-                Spacer(modifier = Modifier.padding(8.dp))
-                Text(
-                    buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                fontSize = 18.sp,
-                                fontWeight = FontWeight.Bold,
-                                fontFamily = FontFamily.Monospace,
-                            )
-                        ) {
-                            append("locación:")
-                        }
-                        withStyle(
-                            style = SpanStyle(
-                                fontSize = 15.sp,
-                                fontWeight = FontWeight.SemiBold,
-                                fontFamily = FontFamily.Monospace,
-                            )
-                        ) {
-                            append(charactersStates.character!!.results[id].location.name)
-                        }
-
-                    }
-                )
-                Spacer(modifier = Modifier.padding(8.dp))
-                Text(
-                    buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                fontSize = 18.sp,
-                                fontWeight = FontWeight.Bold,
-                                fontFamily = FontFamily.Monospace,
-                            )
-                        ) {
-                            append("creado:")
-                        }
-                        withStyle(
-                            style = SpanStyle(
-                                fontSize = 15.sp,
-                                fontWeight = FontWeight.SemiBold,
-                                fontFamily = FontFamily.Monospace,
-                            )
-                        ) {
-                            append(charactersStates.character!!.results[id].created)
-                        }
-
-                    }
-                )
-            }
+            TextViewDescription(
+                atribute = "Genero: ",
+                description = charactersStates.character!!.results[id].gender
+            )
+            TextViewDescription(
+                atribute = "tipo: ",
+                description = charactersStates.character!!.results[id].type
+            )
+            TextViewDescription(
+                atribute = "locación: ",
+                description = charactersStates.character!!.results[id].location.name
+            )
+            TextViewDescription(
+                atribute = "creado: ",
+                description = charactersStates.character!!.results[id].created
+            )
         }
     }
+
+}
+
+@Composable
+fun TextViewDescription(atribute: String, description: String) {
+    Spacer(modifier = Modifier.padding(8.dp))
+    Text(
+        buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Monospace,
+                )
+            ) {
+                append(atribute)
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = FontFamily.Monospace,
+                )
+            ) {
+                append(description)
+            }
+
+        }
+    )
 }
